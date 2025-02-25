b5551628251188b885813b381240ddb0d4eaa165
sxbject <2007kirill02@gmail.com>
2025-02-25 17:30:15 +0300

    Delete game directory

--- game/map/point.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-"""
-Хранения очков в уровне или Аркаде
-"""
-
-Point = namedtuple('Point', ['x', 'y'])
--- game/visual/hit.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-"""
-Связка удара по объекту
-"""
-
-SpriteHit = namedtuple('SpriteHit', ['sprite_index', 'distance', 'angel', 'casted_ray_index', 'texture'])
-RayCastHit = namedtuple('RayCastHit', ['distance', 'point', 'angel', 'offset'])
